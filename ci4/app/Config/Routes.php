ab156c7c4eb052fe73c2c64fa4ff451970c3c186
Marc Angelo A. Nerez <manerez@student.apc.edu.ph>
2024-03-01 08:56:13 +0800

    The guest list

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -5,9 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->get('/', 'Home::index');
 
-use App\Controllers\News; // Add this line
+use App\Controllers\Guests;
+use App\Controllers\News;
 use App\Controllers\Pages;
 
+
+$routes->get('Guests', [Guests::class, 'index']);           // Add this line
+$routes->get('Guests/(:segment)', [Guests::class, 'show']); // Add this line
 
 $routes->get('news', [News::class, 'index']); 
 $routes->get('news/new', [News::class, 'new']);
