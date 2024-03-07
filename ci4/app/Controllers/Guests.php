1bc52730fb5e4b593007115961cdcbed8713c5e2
Marc Angelo A. Nerez <manerez@student.apc.edu.ph>
2024-03-07 14:09:45 +0800

    Fixing the controllers in guests

--- ci4/app/Controllers/Guests.php
+++ ci4/app/Controllers/Guests.php
@@ -9,21 +9,21 @@ class Guests extends BaseController
 {
     public function index()
     {
-        $model = model(GuestsModel::class);
+        $model = new GuestsModel(); // Instantiate the GuestsModel class
 
         $data = [
-            'guests'  => $model->getGuests(),
+            'guests' => $model->getGuests(),
             'title' => 'Guest Logbook',
         ];
 
         return view('templates/header', $data)
-            . view('guests/index')
+            . view('guests/index', $data) // Pass data to the view
             . view('templates/footer');
     }
 
     public function show($email = null)
     {
-        $model = model(GuestsModel::class);
+        $model = new GuestsModel(); // Instantiate the GuestsModel class
 
         $data['guests'] = $model->getGuests($email);
 
@@ -34,7 +34,7 @@ class Guests extends BaseController
         $data['title'] = $data['guests']['name'];
 
         return view('templates/header', $data)
-            . view('guests/view')
+            . view('guests/view', $data) // Pass data to the view
             . view('templates/footer');
     }
 
@@ -54,11 +54,11 @@ class Guests extends BaseController
         $data = $this->request->getPost(['name', 'email', 'website', 'comment', 'gender']);
 
         // Checks whether the submitted data passed the validation rules.
-        if (! $this->validateData($data, [
+        if (! $this->validate([
             'name' => 'required|max_length[255]|min_length[3]',
             'email' => 'required|max_length[255]|min_length[3]',
             'website' => 'required|max_length[255]|min_length[3]',
-            'comment'  => 'required|max_length[5000]|min_length[10]',
+            'comment' => 'required|max_length[5000]|min_length[10]',
             'gender' => 'required|max_length[255]|min_length[3]',
         ])) {
             // The validation fails, so returns the form.
@@ -66,9 +66,9 @@ class Guests extends BaseController
         }
 
         // Gets the validated data.
-        $post = $this->validator->getValidated();
+        $post = $this->request->getPost();
 
-        $model = model(GuestsModel::class);
+        $model = new GuestsModel(); // Instantiate the GuestsModel class
 
         $model->save([
             'name' => $post['name'],
@@ -78,7 +78,7 @@ class Guests extends BaseController
             'gender' => $post['gender'],
         ]);
 
-	header("refresh: 3");
+        header("refresh: 3");
 
         return view('templates/header', ['title' => 'Create a guest item'])
             . view('guests/success')
