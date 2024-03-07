3b4acd1fb514a88d8ea6578475f3932dfeef541b
Marc Angelo A. Nerez <manerez@student.apc.edu.ph>
2024-03-07 14:17:39 +0800

    Fixing the Guestmodel

--- ci4/app/Models/GuestModel.php
+++ ci4/app/Models/GuestModel.php
@@ -4,17 +4,26 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class NewsModel extends Model
+class GuestModel extends Model
 {
     protected $table = 'myguests';
 	
-	
-	public function getGuest($email = false)
+    public function getGuest($email = false)
     {
         if ($email === false) {
             return $this->findAll();
         }
 
         return $this->where(['email' => $email])->first();
+    }
+
+    public function saveGuest($data)
+    {
+        return $this->insert($data);
+    }
+
+    public function updateGuest($email, $data)
+    {
+        return $this->update(['email' => $email], $data);
     }
 }
